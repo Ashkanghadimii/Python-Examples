bbae202198f7657f82d177eb257a1bfa83769eae
Ashkan <159788694+Ashkanghadimii@users.noreply.github.com>
2024-02-29 10:53:09 +0100

    Update username file.py

--- username file.py
+++ username file.py
@@ -5,19 +5,30 @@ captcha=""
 for i in range(5):
     n=random.choice(L)
     captcha=captcha+n
-while True:
-    a=input("sign up or else?")
-    if a.lower()=="sign up":
-        user=input("Enter your username:")
-        password=input("Enter your pass:")
-
-        print(captcha)
-        b=input("Enter the captcha:")
-        if b==captcha:
-            file.write(user+","+password+"\n")
-        else:
-            print("Captcha is wrong")
+a=input("sign up or sign in?")
+if a.lower()=="sign up":
+    user=input("Enter your username:")
+    password=input("Enter your pass:")
+    print(captcha)
+    b=input("Enter the captcha:")
+    if b==captcha:
+        file.write(user+","+password+"\n")
     else:
-        print("Goodbye")
-        break
+        print("Captcha is wrong")
+else:
+    print("go to the log in system below")
 
+print("-----------------------Login System--------------------------------")
+file = open("data.csv","r")
+text=file.read()
+login=input("do you want to login into your account?")
+if login.lower() == "yes":
+    login_user = input("Enter your username:")
+    login_password = input("Enter your pass:")
+    if login_user in text and login_password in text:
+        print("Login was successful")
+    else:
+        print("Username or Password was incorrect")
+else:
+    print("Goodbye")
+file.close()
