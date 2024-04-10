573b7af8c39b6c6242670e788aa9722153878d41
Ashkan <159788694+Ashkanghadimii@users.noreply.github.com>
2024-04-10 12:29:41 +0200

    Create Trip_Condition.py

---
+++ Trip_Condition.py
@@ -0,0 +1,11 @@
+L = []
+while True:
+    Name = input("Enter your Trips conditions: ")
+    if Name[0] == "Y":
+        print("Haji")
+    elif Name[1] == "Y":
+        print("Karbalaee")
+    elif Name[2] == "Y":
+        print("Mashti")
+    else:
+        print("Agha")
